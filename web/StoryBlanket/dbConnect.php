2e469c7f46f0e81ae3451532f321f2158835387b
Katherine Hobbs <Katherine Hobbs>
2017-02-11 16:55:06 -0700

    removed test code for dbconnect

--- web/StoryBlanket/dbConnect.php
+++ web/StoryBlanket/dbConnect.php
@@ -1,17 +1,13 @@
 <?php
-echo "php loaded";
 function get_db() {
 	$dbUrl = "postgres://rpdblhqsnnjkab:65a4ba798295b69ce1a2d2dd984c7debf0a9783d8ab46388367cb75aedb0a431@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d72ipoofbb416q";
-    echo "URL: $dbUrl";
 	if (empty($dbUrl)) {
 		$dbUser = "blanket_user";
 		$dbPassword = "story";
 		$dbPort = "5432";
 		$dbHost = "localhost";
 		$dbName = "storyblanket";
-        echo "here";
 	} else {
-        echo "success";
 		$dbopts = parse_url($dbUrl);
 		$dbHost = $dbopts["host"];
 		$dbPort = $dbopts["port"];
@@ -26,6 +22,6 @@ function get_db() {
 	 print "<p>error: $ex->getMessage() </p>\n\n";
 	 die();
 	}
-//	return $db;
+	return $db;
 }
 ?>
