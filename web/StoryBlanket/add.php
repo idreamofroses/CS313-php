15876628caba9b7615e65a5da6099af83f764e05
Katherine Hobbs <Katherine Hobbs>
2017-02-11 17:10:08 -0700

    testing new image url

--- web/StoryBlanket/add.php
+++ web/StoryBlanket/add.php
@@ -22,21 +22,20 @@ $db = get_db();
 echo "username: $username<br>";
 echo "id: $id<br>";
 echo "title: $title<br>";
-//echo "<img src='$image'><br>";
+echo "<img src='$image'><br>";
 echo "size: $size<br>";
 echo "time: $time <br>";
 
-$statement = $db->prepare("INSERT INTO pattern(pattern_title, pattern_img, time_required, blanket_type, created_by, story)
-VALUES(:title, :img, :time, :size, :user, :story);");
+/*$statement = $db->prepare("INSERT INTO pattern(pattern_title, pattern_img, time_required, blanket_type, created_by)
+VALUES(:title, :img, :time, :size, :user);");
 $statement->bindValue(":title", $title, PDO::PARAM_STR);
 $statement->bindValue(":img", $image, PDO::PARAM_STR);
 $statement->bindValue(":time", $time, PDO::PARAM_INT);
 $statement->bindValue(":size", $size, PDO::PARAM_INT);
 $statement->bindValue(":user", $id, PDO::PARAM_INT);
-$statement->bindValue(":story", $story, PDO::PARAM_STR);
 $statement->execute(); 
 
 header("Location: myPatterns.php?Browse My Patterns");
-die();
+die(); */
 
 ?>
